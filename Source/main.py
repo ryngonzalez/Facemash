eff323596a505d4889f10f28878f0ab1ff908b41
rmg7344 <rmg7344@gmail.com>
2011-10-26 08:41:21 -0400

    Let's make something great.

--- Source/main.py
+++ Source/main.py
@@ -4,3 +4,34 @@
 #
 #
 
+import pygame, cv
+# import settings
+# import play
+# import learn
+
+class Game():
+
+	def __init__(self):
+		pygame.init()
+		screen = pygame.display.set_mode(pygame.FULLSCREEN)
+		pygame.display.set_caption('Facemash')
+	def event(self, event):
+		if event.type == QUIT:
+			sys.exit()
+		elif event.type == KEYDOWN:
+			if event.key == K_ESCAPE:
+				sys.exit()
+
+def main():
+	game = Game()
+	clock = pygame.time.Clock()
+	running = True
+	while running:
+		clock.tick(60)
+		pygame.display.flip()
+		for event in pygame.event.get():
+			game.event(event)
+
+# Run the main program now.			
+main()
+
